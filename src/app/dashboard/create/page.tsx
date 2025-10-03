8ea6b5c098d4ea1a26b8cbdf29ef1d41fe002906
Sean Coding <104483622+lashawty@users.noreply.github.com>
2025-10-03 11:50:39 +0800

    feat: 新增路由架構

---
+++ src/app/dashboard/create/page.tsx
@@ -0,0 +1,6 @@
+// 柚子
+// 麻煩協助開發 建立任務表單
+
+export default function CreateOrganizationPage() {
+  return <div>建立任務表單</div>;
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
-export default async function DashboardPage() {
-  return <div className="space-y-5 p-4 h-full">登入後第一內頁，尚未開發</div>;
+import { Button } from "@/components/ui/button";
+import { ChartNoAxesColumn, Plus } from "lucide-react";
+import Link from "next/link";
+
+export default function OrganizationPage() {
+  return (
+  <div>
+    <p>組織頁面: 顯示管理任務列表 點擊新增、或是任務會進入到各自內頁</p>
+    <Link href="/dashboard/create">
+    <Button>新增任務 <Plus /></Button></Link>
+    <Link href="/dashboard/1">
+    <Button>任務詳細頁面 <ChartNoAxesColumn /></Button></Link>
+  </div>);
 }
